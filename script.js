9ea4d232ef1005bf754c19f415a7e08670e35519
New Name <new@email.com>
2026-07-31 00:55:31 +0000

    yuh

--- script.js
+++ script.js
@@ -8,5 +8,41 @@ setInterval(addCps, 1000);
 function addCps() {
     candies += cps;
     var candieText = document.getElementById("candieText");
-    candieText.textContent = "you have " + candies + " candies";
+    candieText.textContent = "you have " + Math.round(candies) + " candies";
+    for (var i = 0; i < upgrades.length; i++) {
+        if (candies >= upgrades[i].cost / 2) {
+            var upg = document.createElement("button");
+            upg.textContent = "Buy " + upgrades[i].name + " for " + upgrades[i].cost + " candies";
+            upg.onclick = function() {
+                upgrades[i].buy();
+            };
+            var upsec = document.getElementById("upsec");
+            upsec.appendChild(upg);
+        }
+    }
 }
+
+class upgrade {
+    constructor(name, cost, cps) {
+        this.name = name;
+        this.cost = cost;
+        this.cps = cps;
+    }
+
+    buy() {
+        if (candies >= this.cost) {
+            candies -= this.cost;
+            cps += this.cps;
+            var candieText = document.getElementById("candieText");
+            candieText.textContent = "you have " + candies + " candies";
+        } else {
+            alert("Not enough candies to buy " + this.name);
+        }
+    }
+}
+
+var upgrades = [
+    new upgrade("chained slaves", 10, 0.5),
+    new upgrade("oatmeal breakfast", 50, 1),
+    new upgrade("white monster", 100, 2)
+];
