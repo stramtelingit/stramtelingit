1803f778fa79f24aee6a0c63bc092ec7d2abe22e
stramtelingit <lekshang25@wai-int.school.nz>
2026-08-07 00:32:25 +0000

    yuh

--- upg_functions.js
+++ upg_functions.js
@@ -4,3 +4,9 @@ window.doom = function (upg) {
     console.log("Special upgrade used:", upg.name);
     // your custom logic here
 };
+
+window.doom2 = function (upg, text) {
+    console.log("Special upgrade used:", upg.name);
+    para = text;
+    para.textContent = "You have unlocked the doom upgrade!";
+};
