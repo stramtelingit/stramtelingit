9fe65de2d17dd17cc69e14f9cb82d9e34f3208ec
New Name <new@email.com>
2026-08-21 00:21:41 +0000

    yuh

--- upg_functions.js
+++ upg_functions.js
@@ -10,3 +10,9 @@ window.doom2 = function (upg, text) {
     para = document.getElementById(text);
     para.textContent = "You have unlocked the doom upgrade!";
 };
+
+window.unlock_chocolate = function (upg) {
+    console.log("Special upgrade used:", upg.name);
+    window.farm_active = true;
+    window.chocolate_cps = 1;
+};
