5d5842cc8bdc0ce3e6d779577c023ecb6f3916d2
stramtelingit <lekshang25@wai-int.school.nz>
2026-09-11 00:33:08 +0000

    13 alpha 5 hotfix 1

--- js/unlock.js
+++ js/unlock.js
@@ -38,3 +38,23 @@ window.unlock_upgrades_kevin = function(general_upgrades) {
         }
     }
 }
+
+window.unlock_upgrades_kevin_chocolates = function(general_upgrades) {
+    var generalUpgrades = general_upgrades;
+    for (let i = 0; i < generalUpgrades.length; i++) {
+        if (chocolates >= (generalUpgrades[i].cost / 2)) {
+            if (generalUpgrades[i].unlocked == false){
+                var upg = document.createElement("button");
+                const currentUpgrade = generalUpgrades[i];
+                upg.textContent = "Buy " + generalUpgrades[i].name + " for " + (generalUpgrades[i].cost + 1) + " chocolates";
+                upg.onclick = function() {
+                    buyUpg(currentUpgrade);
+                };
+                upg.id = generalUpgrades[i].name;
+                generalUpgrades[i].unlocked = true;
+                var upsec = document.getElementById("kevin");
+                upsec.appendChild(upg);
+            }
+        }
+    }
+}
